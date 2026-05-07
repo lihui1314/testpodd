4ce706de7d30c50e80ece4cb35e4d686bf63ade4
李辉 <1553361562@163.com>
2026-05-07 20:09:07 +0800

    1

---
+++ Podfile
@@ -0,0 +1,5 @@
+use_frameworks!
+platform :ios, '13.0'
+target 'testpodd' do
+  pod 'SnapKit'
+end
---
+++ testpodd/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testpodd
+//
+//  Created by 李辉 on 2026/5/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
